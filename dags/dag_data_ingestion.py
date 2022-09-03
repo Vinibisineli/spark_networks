f50983b0dd7c80ad7ec4747993e372f1f451d5d3
Vinicius Bisineli Gonaçalves <vinicius.goncalves@eleflow.com.br>
2022-09-02 22:56:00 -0300

    coding check http and method get

--- dags/dag_data_ingestion.py
+++ dags/dag_data_ingestion.py
@@ -1,8 +1,11 @@
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.operators.python_operator import PythonOperator
 
 from datetime import datetime,timedelta
+
 
 default_args = {
     'depends_on_past': False,
@@ -24,15 +27,27 @@ with DAG(
         task_id = 'start'
     )
 
-    http_user_check_sensor = SimpleHttpOperator(
+    http_user_check_sensor = HttpSensor(
         task_id='api_user_checker',
+        http_conn_id='http_xpto_api',
+        endpoint='evaluation/dataengineer/jr/v1/users'
+    )
+
+    http_message_check_sensor = HttpSensor(
+        task_id='api_message_checker',
+        http_conn_id='http_xpto_api',
+        endpoint='evaluation/dataengineer/jr/v1/messages'
+    )
+
+    http_user_get = SimpleHttpOperator(
+        task_id='api_user_get',
         http_conn_id='http_xpto_api',
         method='GET',
         endpoint='evaluation/dataengineer/jr/v1/users'
     )
 
-    http_message_check_sensor = SimpleHttpOperator(
-        task_id='api_message_checker',
+    http_message_get = SimpleHttpOperator(
+        task_id='api_message_get',
         http_conn_id='http_xpto_api',
         method='GET',
         endpoint='evaluation/dataengineer/jr/v1/messages'
@@ -42,4 +57,6 @@ with DAG(
         task_id = 'end'
     )
 
-    start >> [http_user_check_sensor,http_message_check_sensor] >> end
+    start >> [http_user_check_sensor,http_message_check_sensor] 
+    http_user_check_sensor >> http_user_get >> end
+    http_message_check_sensor >> http_message_get >> end
